8d3a2295196c3ee9bb5789d3ea5ecdb38ad5c360
HydroTuConnais <hydrotuconnais@gmail.com>
2025-01-28 00:25:37 +0100

    Initial Commit

--- backend/prisma/migrations/20250114233929_addoffset/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Document" ALTER COLUMN "offsety" SET DATA TYPE DOUBLE PRECISION;
--- backend/prisma/migrations/20250120111158_add_boarding/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "boardingStatus" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "boardingStep" INTEGER NOT NULL DEFAULT 1;
